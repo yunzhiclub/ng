2bd13abd97216ea5d7e79de2628966a1dcf3e505
teacherpan <3792535@qq.com>
2021-02-23 14:37:31 +0800

    readme

--- projects/sample/src/app/user.api.ts
+++ projects/sample/src/app/user.api.ts
@@ -7,15 +7,33 @@ import {User} from './user';
 // 集成测试请启用如下代码
 import {MockApiService, MockApiInterface} from '@yunzhi/ng-mock-api';
 
+/**
+ * user模拟接口.
+ * 必须实现MockApiInterface接口
+ */
 export class UserApi implements MockApiInterface {
-  injectMockHttpService(mockHttpService: MockApiService): void {
-    console.log('regsiter');
-    mockHttpService.registerMockApi<User>('PUT', `^user/(\\d+)$`,
-      (next, urlMatches, options) => {
-        return new Observable<HttpResponse<User>>(observable1 => {
+
+  /**
+   * 实现接口MockApiInterface中的injectMockHttpService方法
+   * @param mockApiService 模拟API服务
+   */
+  injectMockHttpService(mockApiService: MockApiService): void {
+
+    /**
+     * 开始注册更新用户接口.
+     * 请求方法为:put
+     * url为: user/123
+     */
+    mockApiService.registerMockApi<User>('PUT', `^user/(\\d+)$`,
+      // handler为该接口对应返回的模块数据
+      (delayNext, urlMatches, options) => {
+        return new Observable<HttpResponse<User>>(observable => {
+          // 用于延迟发送数据的delayNext
+          console.log(delayNext);
+          // 获取到的URL信息
           console.log(urlMatches);
+          // 其它请求选项
           console.log(options);
-          console.log(next);
 
           // 获取参数
           const id = +urlMatches[1];
@@ -24,8 +42,9 @@ export class UserApi implements MockApiInterface {
           const body = options.body as User;
           body.id = id;
 
-          observable1.next(new HttpResponse({body}));
-          observable1.complete();
+          // 响应请求
+          observable.next(new HttpResponse({body}));
+          observable.complete();
         });
       });
   }
