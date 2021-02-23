780d9a68e6cbbf20509ff1fe46924deb6eed43c6
teacherpan <3792535@qq.com>
2021-02-23 12:55:57 +0800

    exports types

--- projects/mock-api/src/public-api.ts
+++ projects/mock-api/src/public-api.ts
@@ -5,3 +5,4 @@ export * from './lib/utils';
 export * from './lib/mock-api.interface';
 export * from './lib/mock-api.interceptor';
 export * from './lib/mock-api.service';
+export * from './lib/mock-api.types';
