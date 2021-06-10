9e04be50102705093e065064cb724df75ece843d
panjie <3792535@qq.com>
2021-06-10 13:52:06 +0800

    完善个写法

--- projects/mock-api/src/lib/mock-api.service.ts
+++ projects/mock-api/src/lib/mock-api.service.ts
@@ -162,7 +162,7 @@ export class MockApiService {
     if (keys.length === 0) {
       return new Observable<HttpErrorResponse>(subscriber => {
         const message = `yzMockApi Error: can't find mock result data:` +
-          `1. pls make sure the request's 'url'(${url}) and 'method'(${method}) is right.` +
+          `1. pls make sure the request's url '${url}' and method '${method}' is right.` +
           `2. pls make sure your mockApi file has been added to the module HttpInterceptor.`;
         console.error(message);
         console.log('hello');
