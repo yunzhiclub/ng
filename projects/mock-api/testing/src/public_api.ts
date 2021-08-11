254fe4ed4b931139f0a2cdce2b2fa16f6fc605f4
teacherpan <3792535@qq.com>
2021-08-11 10:08:52 +0800

    add angular 12

--- projects/mock-api/testing/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-/*
- * Public API Surface of mock-http-client testing
- */
-export * from './utils-test';
-export * from './lib/mock-api.testing.interceptor';
--- projects/router-testing/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of router-testing
- */
-
-export * from './lib/router-testing.module';
-export * from './lib/activated-route.stub';
-export * from './lib/router.stub';
-
