322dbe3be7990faeb416cdcf8dbb17400b1da859
teacherpan <3792535@qq.com>
2021-08-12 22:58:39 +0800

    remove router

--- angular12/projects/sample/src/app/app.component.ts
+++ angular12/projects/sample/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from './user.service';
-import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +12,7 @@ export class AppComponent implements OnInit {
   loginResult = '';
   networkErrorPass = false;
 
-  constructor(private userService: UserService,
-              private router: Router,
-              private route: ActivatedRoute) {
+  constructor(private userService: UserService) {
   }
 
   ngOnInit(): void {
