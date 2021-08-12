a18d8831ff13cc798e435773012d18ce4af44f61
teacherpan <3792535@qq.com>
2021-08-12 23:06:49 +0800

    export

--- angular12/projects/sample/src/app/router.spec.ts
+++ angular12/projects/sample/src/app/router.spec.ts
@@ -1,5 +1,5 @@
 import {TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@yunzhi/ng-router-testing';
+import {RouterTestingModule, RouterStub, ActivatedRouteStub} from '@yunzhi/ng-router-testing';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 
 
@@ -12,8 +12,8 @@ describe('Router route测试', () => {
 
   it('router 与 route的配合', () => {
     let params: Params;
-    const router = TestBed.inject(Router);
-    const route = TestBed.inject(ActivatedRoute);
+    const router = TestBed.inject(Router) as unknown as RouterStub;
+    const route = TestBed.inject(ActivatedRoute) as unknown as ActivatedRouteStub;
     console.log(router);
     console.log(route);
     route.queryParams.subscribe(p => {
