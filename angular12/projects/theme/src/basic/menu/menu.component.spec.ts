66f0ddb7c0e430d99fcee169d848a379f528e819
panjie <3792535@qq.com>
2021-08-30 17:05:16 +0800

    添加子菜单

--- angular12/projects/theme/src/basic/menu/menu.component.spec.ts
+++ angular12/projects/theme/src/basic/menu/menu.component.spec.ts
@@ -2,8 +2,10 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MenuComponent} from './menu.component';
 import {RouterTestingModule} from '@angular/router/testing';
-import {BasicService} from '../service/basic.service';
 import {ApiModule} from '../api/api.module';
+import {BasicService} from '../service/basic.service';
+import {Observable} from 'rxjs';
+import {Menu} from '../entity/menu';
 
 describe('MenuComponent', () => {
   let component: MenuComponent;
@@ -15,8 +17,8 @@ describe('MenuComponent', () => {
       imports: [
         ApiModule,
         RouterTestingModule
-      ],providers: [
-        BasicService
+      ], providers: [
+        {provide: BasicService, useClass: MyBasicService}
       ]
     })
       .compileComponents();
@@ -28,9 +30,54 @@ describe('MenuComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
-    component.links.first.nativeElement.className += 'active';
+    console.log(component.links);
+    component.links.get(1).nativeElement.className += 'active';
     fixture.detectChanges();
   });
+
+  it('getColorWithRadix', () => {
+    expect('rgba(251,175,255,1)').toEqual(MenuComponent.hexToRgbA('#fbafff', 1));
+  })
 });
+
+
+class MyBasicService extends BasicService {
+  /**
+   * 获取菜单
+   */
+  getMenus(): Observable<Menu[]> {
+    console.log('重此getMenus()实现自定义菜单');
+    return new Observable(subscribe => {
+      subscribe.next([{
+        name: '首页',
+        url: 'dashboard',
+        icon: 'fa fa-tachometer-alt',
+      }, {
+        name: '父子菜单',
+        url: '',
+        icon: 'fa fa-tachometer-alt',
+        children: [{
+          name: '首页',
+          url: 'sub/dashboard',
+          icon: 'fa fa-tachometer-alt',
+        }, {
+          name: '模板页',
+          url: 'sub/theme',
+          icon: 'fa fa-tachometer-alt',
+        }, {
+          name: '模板页',
+          url: 'sub/theme',
+          icon: 'fa fa-tachometer-alt',
+        }]
+      }, {
+        name: '模板页',
+        url: 'theme',
+        icon: 'fa fa-tachometer-alt',
+      }]);
+      subscribe.complete();
+    });
+  }
+}
+
