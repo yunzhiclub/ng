5b6342104ff934a7d5e186332a523a82966839b7
panjie <3792535@qq.com>
2021-08-30 10:44:38 +0800

    ng build

--- angular12/projects/common/src/public-api.ts
+++ angular12/projects/common/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/yz-lists-checkbox/yz-lists-checkbox.module';
+export * from './lib/yz-lists-checkbox/yz-lists-checkbox.component';
 export * from './lib/model/checkbox';
 export * from './lib/model/error-message';
 export * from './lib/model/http-code';
@@ -17,8 +18,13 @@ export * from './lib/model/pageable';
 export * from './lib/model/slice';
 export * from './lib/model/sort';
 export * from './lib/yz-page/yz-page.module';
+export * from './lib/yz-page/yz-page.component';
 export * from './lib/yz-size/yz-size.module';
+export * from './lib/yz-size/yz-size.component';
 export * from './lib/yz-mask/yz-mask.module';
+export * from './lib/yz-mask/yz-mask.component';
 export * from './lib/yz-modal/yz-modal.module';
+export * from './lib/yz-modal/yz-modal.component';
 export * from './lib/yz-uploader/yz-uploader.module';
+export * from './lib/yz-uploader/yz-uploader.component';
 export * from './lib/attachment.service';
