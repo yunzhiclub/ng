ddf47a90a746b1982f181fa8dca8838c45ac94d4
panjie <3792535@qq.com>
2021-12-13 16:28:21 +0800

    为菜单添加描述信息

--- angular12/projects/theme/src/basic/entity/menu.ts
+++ angular12/projects/theme/src/basic/entity/menu.ts
@@ -2,12 +2,24 @@
  * 菜单格式
  */
 export class Menu {
-  name: string;
-  url: string;
-  icon: string;
-
   /**
    * 子菜单
    */
   children?: Menu[] = [];
+  /**
+   * 描述信息
+   */
+  description?: string;
+  /**
+   * 图标的样式
+   */
+  icon: string;
+  /**
+   * 显示的名称
+   */
+  name: string;
+  /**
+   * 跳转的URL地址
+   */
+  url: string;
 }
