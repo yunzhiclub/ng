bcc668db0fb42e3711c6fcf30716e155af279867
panjie <3792535@qq.com>
2024-12-04 13:46:49 +0800

    add mockapi and fix some bug

--- angular19/src/app/app.component.ts
+++ angular19/src/app/app.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BasicComponent, ThemeService } from '../../projects/theme/src/public-api';
+import { BasicComponent, ThemeService, YzMenu } from '../../projects/theme/src/public-api';
+import { Observable } from 'rxjs';
 
 export class MyThemeService extends ThemeService {
-  // 在此复写
+  public constructor() {
+    super();
+    console.log('hello');
+  }
+
+  /**
+   * 获取菜单
+   */
+  override getMenus(): Observable<YzMenu[]> {
+    console.log('重此getMenus()实现自定义菜单');
+    return new Observable(subscribe => {
+      subscribe.next([{
+        name: '首页1',
+        url: 'dashboard',
+        icon: 'fa fa-tachometer-alt',
+      }, {
+        name: '父子菜单1',
+        url: '',
+        icon: 'fa fa-tachometer-alt',
+        children: [{
+          name: '首页',
+          url: 'sub/dashboard',
+          icon: 'fa fa-tachometer-alt',
+        }, {
+          name: '模板页',
+          url: 'sub/theme',
+          icon: 'fa fa-tachometer-alt',
+        }, {
+          name: '模板页',
+          url: 'sub/theme',
+          icon: 'fa fa-tachometer-alt',
+        }]
+      }, {
+        name: '模板页',
+        url: 'theme',
+        icon: 'fa fa-tachometer-alt',
+      }]);
+      subscribe.complete();
+    });
+  }
 }
 
 @Component({
